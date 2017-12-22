f097d62fadce7b523f05e4f4fb42441ea44539d6
aetheryx <zain1@live.nl>
2017-12-23 00:16:23 +0100

    Update set to use message collectors

--- discord/commands/set.js
+++ discord/commands/set.js
@@ -1,11 +1,33 @@
-exports.run = async function set (Bot, msg, args) {
-  if (!args[1]) {
-    return msg.channel.send('Missing required arguments.');
+exports.run = async function set (Bot, msg) {
+  msg.channel.send('What\'s the trigger of the response you\'d like to edit?');
+
+  const cancel = () => msg.channel.send('Cancelled.');
+
+  let name = await msg.channel.awaitMessages(message => message.author.id === msg.author.id, { max: 1, time: 30000 });
+  if (name) {
+    name = name.first().content;
+    if (name === 'cancel') {
+      return cancel();
+    }
+  } else {
+    return;
   }
-  
-  Bot.updateResponses({ [args[0]]: args[1] });
-  msg.channel.send('gud');
-}
+
+  msg.channel.send(`What's the new content of \`${name}\`?`);
+  let content = await msg.channel.awaitMessages(message => message.author.id === msg.author.id, { max: 1, time: 30000 });
+  if (content) {
+    content = content.first().attachments.first() ? content.first().attachments.first().url : content.first().content;
+    if (content === 'cancel') {
+      return cancel();
+    }
+  } else {
+    return;
+  }
+
+
+  Bot.updateResponses({ [name.replace(/ /g, '')]: content });
+  msg.channel.send(':ok_hand:');
+};
 
 exports.props = {
   name: 'set',
