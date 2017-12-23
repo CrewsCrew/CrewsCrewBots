17a96c5a50f68b492d44bef0dc50152f1d699dde
aetheryx <zain1@live.nl>
2017-12-23 01:13:11 +0100

    add permission check

--- discord/commands/set.js
+++ discord/commands/set.js
@@ -1,11 +1,15 @@
-exports.run = async function set (Bot, msg) {
+exports.run = async function SetCommand (Bot, msg) {
+  if (!msg.member.roles.has(Bot.config.roleID)) {
+    return msg.channel.send('You are not authorized to use this command.');
+  }
+
   msg.channel.send('What\'s the trigger of the response you\'d like to edit?');
 
   const cancel = () => msg.channel.send('Cancelled.');
 
   let name = await msg.channel.awaitMessages(message => message.author.id === msg.author.id, { max: 1, time: 30000 });
   if (name) {
-    name = name.first().content;
+    name = name.first().content.replace(/ /g, '');
     if (name === 'cancel') {
       return cancel();
     }
@@ -16,7 +20,8 @@ exports.run = async function set (Bot, msg) {
   msg.channel.send(`What's the new content of \`${name}\`?`);
   let content = await msg.channel.awaitMessages(message => message.author.id === msg.author.id, { max: 1, time: 30000 });
   if (content) {
-    content = content.first().attachments.first() ? content.first().attachments.first().url : content.first().content;
+    content = content.first();
+    content = content.attachments.first() ? content.attachments.first().url : content.content;
     if (content === 'cancel') {
       return cancel();
     }
@@ -24,8 +29,7 @@ exports.run = async function set (Bot, msg) {
     return;
   }
 
-
-  Bot.updateResponses({ [name.replace(/ /g, '')]: content });
+  Bot.updateResponses({ [name]: content });
   msg.channel.send(':ok_hand:');
 };
 
